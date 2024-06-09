f37dfcd7ddaa3017566fae83be1819a773fc8672
ifeanyi <omejuaifeanyi2@gmail.com>
2024-06-09 15:07:46 +0000

    routes control

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,14 +1,34 @@
-import { AppBar, Typography } from '@material-ui/core';
-import useStyles from './style'
+import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
+import useStyles from './style';
+import memories from '../../images/memories.png';
+import { Link } from 'react-router-dom';
 import React from 'react'
 
 const Navbar = () => {
     const classes = useStyles();
 
+    const user = false;
+
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-        <img className={classes.image} src={memories} alt="icon" height="60" />
+        <div className={classes.brandContainer}>
+            <Typography component={Link} to='/' className={classes.heading} variant="h2" align="center">Memories</Typography>
+            <img className={classes.image} src={memories} alt="icon" height="60" />
+        </div>
+
+        <Toolbar className={classes.toolbar}>
+            {user ? 
+                <div className={classes.profile}>
+                    <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>
+                        {user.result.name.charAt(0)}
+                    </Avatar>
+                    <Typography className={classes.userName} variant='h6'>{user.result.name}</Typography>
+                    <Button variant='contained' color='secondary'>Logout</Button>
+                </div>
+            : (
+                <Button component={Link} to='/auth' variant='contained' color='primary'>Sign In</Button>
+            )}
+        </Toolbar>
     </AppBar>
   )
 }
